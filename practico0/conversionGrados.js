d4853e4bf1e87edae5227137a512ec0339c20e55
1708pmg <gutierrezpm61@gmail.com>
2023-05-27 11:46:59 -0300

    practico 0 finalizado

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,7 @@
+let fahrenheit1 = 50
+let celsius1 = (fahrenheit1 - 32) * 5/9;
+console.log(celsius1);
+
+let fahrenheit2 = 75
+let celsius2 = (fahrenheit2 - 32) * 5/9;
+console.log(celsius2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,17 @@
+let x = 5;
+let y = 7;
+let z = 9;
+let promedio = (x + y + z) / 3;
+console.log(promedio);
+
+let x1 = -2;
+let y1 = -3;
+let z1 = -8;
+let promedio1 = (x1 + y1 + z1) / 3;
+console.log(promedio1);
+
+let x2 = -1.2;
+let y2 = 5.3;
+let z2 = -3.3;
+let promedio2 = (x2 + y2 + z2) / 3;
+console.log(promedio2);
