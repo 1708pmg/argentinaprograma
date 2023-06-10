404059bd0af0e366caac508051a2ee696f0e701c
1708pmg <gutierrezpm61@gmail.com>
2023-06-10 16:55:57 -0300

    ejercicio a revisar

---
+++ practico1/frutas.js
@@ -0,0 +1,8 @@
+let fruta = []
+fruta = ["banana", "manzana", "pera", "kiwi", "naranja"]
+let ultima = fruta[fruta.length-1];
+console.log("La ultima fruta agregada fue: " + ultima);
+let primera = fruta[0];
+console.log ("La primera fruta agregada fue: " + primera);
+
+
