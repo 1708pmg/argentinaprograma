459779df6f2fd0ec9ba8cae3efb7f6a308ab4803
1708pmg <gutierrezpm61@gmail.com>
2023-06-13 18:53:08 -0300

    ejercico terminado

--- practico1/cita.js
+++ practico1/cita.js
@@ -1,10 +1,11 @@
 let cita = "Tres tristes tigres comen trigo en un trigal";
 let substring = "tristes tigres comen";
 let tamañoDeCita = cita.length;
-console.log("El tamaño de cita es: " + tamañoDeCita);
+console.log("El tamaño de cita es: %s" ,tamañoDeCita);
 let indice = cita.indexOf(substring);
-console.log("El indice del substring es: " + (indice));
-let citaRevisada = cita.split(" en un trigal");
+console.log("El indice del substring es: %s", indice);
+let substringDeCita = cita.split(" ");
+let citaRevisada = substringDeCita[0] + " "+substring +" " +substringDeCita[4];
 console.log(citaRevisada);
 
 
