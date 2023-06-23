cf7b97a510b6e8d9fb2e195c342f39a6ddf99aea
1708pmg <gutierrezpm61@gmail.com>
2023-06-22 21:27:40 -0300

    Ejercicio terminado

---
+++ practico2/operadoresLogicos.js
@@ -0,0 +1,7 @@
+const readlineSync = require('readline-sync');
+let numero = readlineSync.question("Ingrese un numero entero: ");
+if (numero >= 1 && numero ==(numero / 2)== 0){console.log ("El numero es positivo y par");}
+else if (numero >= 1 && numero !== (numero / 2)=== 0) 
+{console.lor ("El numero es positivo e impar");}
+else if (numero < 0){console.log ("El numero es negativo");}
+else {console.log ("El numero es cero");}
