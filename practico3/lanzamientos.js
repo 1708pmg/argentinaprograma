9975078108756ddf68ab95f4fa69646608b1ae3d
1708pmg <gutierrezpm61@gmail.com>
2023-06-28 18:39:16 -0300

    Lanzamientos terminado

--- practico3/lanzamientos.js
+++ practico3/lanzamientos.js
@@ -1,9 +1,10 @@
-let i = 0
+let i = 10
 for (let i = 10; i >= 0 ; i--){
-  console.log("Cuenta regresiva " + i + " ");
-  if (i == 0){console.log("Lanzamiento!" + i + " ")};
+  if (i === 10)
+  console.log("Cuenta regresiva " + i + ".");
+  else if ( i === 0) 
+  console.log("Lanzamiento! ");
+  else 
+  console.log(i);
 }
-
-
-
-
+// No me quedó claro si debia poner tambien el numero 0, en ese caso la linea 6 seira : console.log(i + " Lanzamientos!")
