772da10eb04fbb2daf691ac8182acdd383e32ad1
1708pmg <gutierrezpm61@gmail.com>
2023-06-28 10:59:35 -0300

    ejercico a corregir

--- practico3/listaDeInvitados.js
+++ practico3/listaDeInvitados.js
@@ -1,10 +1,11 @@
 let personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro","Emilia"];
-let rechazados = [];
-let admitidos = [];
+let rechazados = [""];
+let admitidos = [""];
 for (let i = 0; i < personas.length; i++){
-    if (personas [i] == "Jose" && personas[i] =="Sofia"){
-        rechazados.push(personas[i])
-        console.log("La lista de invitados rechados es: " + rechazados + ".");}
+    if (personas [i] == "Jose" || personas[i] == "Sofia"){
+        console.log("La lista de invitados rechazados es: " + rechazados.push(personas[i]) + ".");}
+        else {console.log ("La lista de invitados admitidos es: "+ admitidos.push(personas[i]) + ".");}
 }
+
 
 
